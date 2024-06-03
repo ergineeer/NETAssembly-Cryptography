9b55407d44ede414bd14809e108a451eae9aae3f
Ergin Sezgin <80001281+ergineeer@users.noreply.github.com>
2024-06-03 18:30:34 +0300

    Update RSA_Encryption.m

--- Main/RSA_Encryption.m
+++ Main/RSA_Encryption.m
@@ -16,3 +16,13 @@ fprintf("\nPrivate Key\n%s\n", privateKey);
 plainText = '"This is a test data."';
 fprintf('\nData to be encrypted:\n%s\n', plainText);
 plainBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+rsa.FromXmlString(publicKey);
+cipherBytes = rsa.Encrypt(plainBytes, false);
+
+cipherTextHex = reshape(dec2hex(uint8(cipherBytes))', 1, []);
+fprintf('\nData encrypted (Hexadecimal):\n%s\n', cipherTextHex);
+
+rsa.FromXmlString(privateKey);
+decryptedBytes = rsa.Decrypt(cipherBytes, false);
+decryptedText = System.Text.Encoding.UTF8.GetString(decryptedBytes);
+fprintf('\nData decrypted:\n%s\n', char(decryptedText));
