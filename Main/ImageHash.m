763885009583acb38eaecb3ff489855ff3003b68
Ergin Sezgin <80001281+ergineeer@users.noreply.github.com>
2024-08-07 22:50:35 +0300

    Update ImageHash.m

--- Main/ImageHash.m
+++ Main/ImageHash.m
@@ -2,18 +2,13 @@
 cd(fileparts(matlab.desktop.editor.getActiveFilename))
 clear
 clc
-rng(90)
+rng(90) 
 
-NET.addAssembly('System.Security');
-image = 'ngc6543a.jpg';
-img = imread(image);
-imgBytes = uint8(img(:));
 
-sha256 = System.Security.Cryptography.SHA256Managed();
-hashBytes = uint8(sha256.ComputeHash(imgBytes));
-
-sha256hash = '';
-for i = 1:length(hashBytes)
-    sha256hash = strcat(sha256hash, lower(dec2hex(hashBytes(i), 2)));
-end
-disp(['Hash: ', sha256hash]);
+%% Method 1 - java.security.MessageDigest.getInstance()
+img = imread('ngc6543a.jpg');
+imgBytes = typecast(img(:), 'uint8');
+md = java.security.MessageDigest.getInstance('SHA-256');
+hashBytes_1 = md.digest(imgBytes);
+sha256hash_1 = sprintf('%02x', typecast(hashBytes_1, 'uint8'));
+disp(['Hash: ', sha256hash_1]);
