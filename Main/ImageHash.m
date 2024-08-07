e05ac30e35e8c73233d5ee7d0f2f342fd05cdb1d
Ergin Sezgin <80001281+ergineeer@users.noreply.github.com>
2024-08-07 22:53:54 +0300

    Update ImageHash.m

--- Main/ImageHash.m
+++ Main/ImageHash.m
@@ -12,3 +12,18 @@ md = java.security.MessageDigest.getInstance('SHA-256');
 hashBytes_1 = md.digest(imgBytes);
 sha256hash_1 = sprintf('%02x', typecast(hashBytes_1, 'uint8'));
 disp(['Hash: ', sha256hash_1]);
+
+%% Method 2 - System.Security.Cryptography.SHA256Managed()
+NET.addAssembly('System.Security');
+image = 'ngc6543a.jpg';
+img = imread(image);
+imgBytes = uint8(img(:));
+sha256 = System.Security.Cryptography.SHA256Managed();
+hashBytes_2 = uint8(sha256.ComputeHash(imgBytes));
+
+sha256hash_2 = '';
+for i = 1:length(hashBytes_2)
+    sha256hash_2 = strcat(sha256hash_2, lower(dec2hex(hashBytes_2(i), 2)));
+end
+disp(['Hash: ', sha256hash_2]);
+
