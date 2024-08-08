88a47cb41d933253e289e28a113f3795d2c8db90
Ergin Sezgin <80001281+ergineeer@users.noreply.github.com>
2024-08-08 19:56:59 +0300

    Update ECDH_KeyExchange.m

--- Main/ECDH_KeyExchange.m
+++ Main/ECDH_KeyExchange.m
@@ -8,3 +8,20 @@
 cd(fileparts(matlab.desktop.editor.getActiveFilename));
 clear; clc;
 rng(90);
+
+%% Method 1 - System.Security.Cryptography
+import System.Security.Cryptography.*
+firstParty = ECDiffieHellmanCng();
+secondParty = ECDiffieHellmanCng();
+firstPartyPublicKey = firstParty.PublicKey.ToByteArray();
+secondPartyPublicKey = secondParty.PublicKey.ToByteArray();
+
+firstPartySharedSecretWithSecond = firstParty.DeriveKeyMaterial(CngKey.Import(secondPartyPublicKey, CngKeyBlobFormat.EccPublicBlob));
+secondPartySharedSecretWithFirst = secondParty.DeriveKeyMaterial(CngKey.Import(firstPartyPublicKey, CngKeyBlobFormat.EccPublicBlob));
+sharedSecretMatches = isequal(firstPartySharedSecretWithSecond, secondPartySharedSecretWithFirst);
+
+if sharedSecretMatches == 1
+    disp('Shared secret matches between parties.');
+else
+    disp('Shared secret doesn''t match.');
+end
