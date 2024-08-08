8d021e421fdfd1c75506f3ecb5cf6fef2cfc50b3
Ergin Sezgin <80001281+ergineeer@users.noreply.github.com>
2024-08-08 19:53:28 +0300

    Create ECDH_KeyExchange.m

---
+++ Main/ECDH_KeyExchange.m
@@ -0,0 +1,10 @@
+% % % % % 
+% Elliptic Curve Diffie-Hellman (ECDH) Key Exchange
+% Ergin Sezgin
+% 08/08/2024
+% 
+
+% Initialization
+cd(fileparts(matlab.desktop.editor.getActiveFilename));
+clear; clc;
+rng(90);
